b4c5135203707782e27c08ce6bf373adad932cd0
ankanroy007 <ankanroy007@users.noreply.github.com>
2017-06-06 15:01:05 +0000

    Done

--- build.py
+++ build.py
@@ -1,7 +1,11 @@
 import pandas as pd
 
 
-def solution(dict):
+def solution(dic):
     """
     Enter your code here
     """
+    df = pd.DataFrame(data=dic)
+    return df
+
+solution({'X':[78,85,96,80,86], 'Y':[84,94,89,83,86],'Z':[86,97,96,72,83]})
