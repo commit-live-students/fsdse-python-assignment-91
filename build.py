be2d207a8ef42440ec33d8469c3c6060b438ec6a
adinathauti <adinathauti@gmail.com>
2017-06-22 09:42:12 +0000

    Done

--- build.py
+++ build.py
@@ -1,7 +1,9 @@
 import pandas as pd
 
 
-def solution(dict):
-    """
-    Enter your code here
-    """
+def solution(dic):
+    df = pd.DataFrame(dic)
+    return df
+
+
+solution({'x':[1,2,3],'y':[4,5,6],'z':[7,8,9]})
