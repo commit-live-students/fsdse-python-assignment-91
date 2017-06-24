3aeedf9aed12c7d422e187f35af55dac8e49b14f
pramodbhalerao <pramodbhalerao_01@yahoo.com>
2017-06-24 06:53:55 +0000

    Done

--- build.py
+++ build.py
@@ -1,7 +1,9 @@
 import pandas as pd
 
 
-def solution(dict):
-    """
-    Enter your code here
-    """
+
+def solution(dict_input) :
+    df = pd.DataFrame.from_dict(dict_input)
+    return df
+
+solution({"X":[78,85,96,80,86], "Y":[84,94,89,83,86],"Z":[86,97,96,72,83]})
