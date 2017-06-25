dfbfa24900344a42caf05ab23658a184f42ece1c
birajparikh16 <birajparikh16@gmail.com>
2017-06-25 16:41:40 +0000

    Done

--- build.py
+++ build.py
@@ -1,7 +1,14 @@
 import pandas as pd
 
-
 def solution(dict):
-    """
-    Enter your code here
-    """
+     return pd.DataFrame(dict)
+
+# dic = {'X':[78,85,96,80,86], 'Y':[84,94,89,83,86],'Z':[86,97,96,72,83]}
+# solution(dic)
+# OUTPUT:
+"""     X 	Y 	Z
+   0 	78 	84 	86
+   1 	85 	94 	97
+   2 	96 	89 	96
+   3 	80 	83 	72
+   4 	86 	86 	83"""
