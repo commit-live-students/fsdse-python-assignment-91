c969e97bd2d94c8642c21533d68ddf8e11079af3
NeelkanthMehta <neilstrong2003@yahoo.com>
2017-09-20 15:36:14 +0000

    Done

--- build.py
+++ build.py
@@ -1,7 +1,7 @@
-import pandas as pd
-
-
 def solution(dict):
+    import pandas as pd
     """
     Enter your code here
     """
+    df1 = pd.DataFrame.from_dict(dict,orient='columns',dtype=None)
+    return df1 
